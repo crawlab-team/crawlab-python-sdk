311dc06a5fbf262a98b91d42313fd66bb3fee1da
Marvin Zhang <tikazyq>
2025-01-02 15:55:15 +0800

    refactor: remove unused import of save_items from __init__.py

--- crawlab/__init__.py
+++ crawlab/__init__.py
@@ -1,8 +1,7 @@
 __all__ = [
     "save_item",
-    "save_items",
     "CrawlabPipeline",
 ]
 
-from crawlab.core.item import save_item, save_items
+from crawlab.core.item import save_item
 from crawlab.core.scrapy.pipelines import CrawlabPipeline
